d0cc239013a2818310ebfc0502fbdd37d1412fb6
Aleksandra Gilyova <gilyova.a.v@gmail.com>
2025-06-11 22:19:01 +0300

    Added 11.1-hw

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/AuthApi.java
@@ -1,5 +1,6 @@
 package guru.qa.niffler.api;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import retrofit2.Call;
 import retrofit2.http.*;
 
@@ -15,4 +16,27 @@ public interface AuthApi {
                           @Field("passwordSubmit") String passwordSubmit,
                           @Field("_csrf") String csrf
   );
+
+  @GET("oauth2/authorize")
+  Call<Void> authorize(@Query("response_type") String response_type,
+                       @Query("client_id") String client_id,
+                       @Query("scope") String scope,
+                       @Query("redirect_uri") String redirect_uri,
+                       @Query("code_challenge") String code_challenge,
+                       @Query("code_challenge_method") String code_challenge_method);
+
+  @FormUrlEncoded
+  @POST("login")
+  Call<Void> login(@Field("_csrf") String csrf,
+                     @Field("username") String username,
+                     @Field("password") String password
+  );
+
+  @FormUrlEncoded
+  @POST("oauth2/token")
+  Call<JsonNode> getToken(@Field("code") String code,
+                          @Field(value = "redirect_uri", encoded = true) String redirect_uri,
+                          @Field(value = "code_verifier", encoded = true) String code_verifier,
+                          @Field("grant_type") String grant_type,
+                          @Field("client_id") String client_id);
 }
